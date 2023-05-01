3b7b98894a4110a4a148e60e96556ea0344ff942
Victor H. Faria <victorfaria1590@gmail.com>
2023-05-01 15:56:26 -0300

    Menu without background.

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(scoreText != null)
+        {
+            scoreText.text = "Pontuação máxima\n" + PlayerPrefs.GetInt("Score");
+        }
+    }
+
+    public void Close()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(WaitClose());
+    }
+
+    IEnumerator WaitClose()
+    {
+        yield return new WaitForSeconds(.5f);
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(WaitPlay());
+    }
+
+    IEnumerator WaitPlay()
+    {
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene("Level");
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(WaitReturnToMenu());
+    }
+
+    IEnumerator WaitReturnToMenu()
+    {
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene("Menu");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # SIN394-MathRacer
 Projeto da disciplina SIN394 - Introdução aos Jogos Digitais. Um jogo 2D de carro para ensinar conceitos de matemática e física.
+
+## Configurar Editor Unity para contribuir no projeto.
+Verifique se as seguintes opções estão configuradas no seu editor:
+* Project Settings > Editor > Asset Serialization > Mode > Force Text
+* Project Settings > Version Control > Mode > Visible Meta Files
+
+## Contribuir com repositório.
+### <span style="color:red">Não fazer commits na branch master.</span>
+
+### Clonar Repositório
+>` git clone https://github.com/victorh1590/sin394-math-racer.git `\
+>`cd sin394-math-racer`
+
+### Criar nova branch
+>`git checkout -b <nome-da-branch>`
+
+### Fazer push no branch após commit
+>`git push -u <nome-da-branch>`
+
+### Antes de qualquer pull request fazer
+>`git pull origin master`
+
+### <span style="color:green">Pull requests devem ser feitos pelo github.</span>
+
+### Após merge do seu pull request, para excluir branch localmente:
+>`git pull`\
+>`git branch -D <nome-da-branch>`\
+
+Em seguida o branch deve ser apagado no github caso não vá ser mais utilizado.
